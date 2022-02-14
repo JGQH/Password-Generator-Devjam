15432bd08658dd331dbccfe334b75b9ea1f983eb
Juan Quispe <arch.btw>
2022-02-14 15:49:57 -0500

    Fix location of files
    Currently, the website doesn't use the correct path based on the repo
    Therefore, it has to be changed at build time for production
    Also installs @types/node to use "process" with typescript

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import tsconfigpaths from 'vite-tsconfig-paths'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigpaths()]
+  plugins: [tsconfigpaths()],
+  base: isProduction ? '/Password-Generator-Devjam/' : '/'
 })
